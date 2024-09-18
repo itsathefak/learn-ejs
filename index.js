344a9c24aafb44978886b2625580fc702e469104
itsathefak <athefayubkhan@gmail.com>
2024-09-18 11:21:10 -0400

    using path and setting views to access it from parents directory

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
 
 let port = 3000;
 
 app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
